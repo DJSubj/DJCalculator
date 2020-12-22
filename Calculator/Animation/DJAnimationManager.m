dc1dd6816111fde9e7413331e329e6a47f30e79c
dujun <djsubj0822@163.com>
2020-12-22 17:32:48 +0800

    修改文件结构

--- Calculator/Animation/DJAnimationManager.m
+++ Calculator/Animation/DJAnimationManager.m
@@ -36,58 +36,32 @@ static DJAnimationManager *_manager;
         return _snowLayer;
     }
     
-    // 创建粒子Layer
+    
     _snowLayer = [CAEmitterLayer layer];
-    
-    // 粒子发射位置
     _snowLayer.emitterPosition = CGPointMake(kScreenWidth / 2.0,0);
-    
-    // 发射源的尺寸大小
     _snowLayer.emitterSize = CGSizeMake(kScreenWidth, kScreenHeight);
-    
-    // 发射模式
     _snowLayer.emitterMode = kCAEmitterLayerSurface;
-    
-    // 发射源的形状
     _snowLayer.emitterShape = kCAEmitterLayerLine;
     
-    // 创建雪花类型的粒子
     CAEmitterCell *snowflake = [CAEmitterCell emitterCell];
-    
     snowflake.contentsScale = 1.5;
-    // 粒子的名字
     snowflake.name = @"snow";
-    
-    // 粒子参数的速度乘数因子
     snowflake.birthRate = 20.0;
     snowflake.lifetime = 10.0;
-    
-    // 粒子速度
     snowflake.velocity = 10.0;
-    
-    // 粒子的速度范围
     snowflake.velocityRange = 10;
-    
-    // 粒子y方向的加速度分量
     snowflake.yAcceleration = 30;
-    
-    // 周围发射角度
-    snowflake.emissionRange = 0.5 * M_PI;
-    
-    // 子旋转角度范围
-    snowflake.spinRange = 0.25 * M_PI;
+    snowflake.emissionRange = M_PI_2;
+    snowflake.spinRange = M_PI_4;
     snowflake.contents  = (id)[[UIImage imageNamed:@"snow"] CGImage];
+    snowflake.scaleRange = 0.6f;
+    snowflake.scale = 0.7f;
     
-    // 设置雪花形状的粒子的颜色
     snowflake.color = UIColor.whiteColor.CGColor;
 //    snowflake.redRange = 1.5f;
 //    snowflake.greenRange = 2.2f;
 //    snowflake.blueRange = 2.2f;
     
-    snowflake.scaleRange = 0.6f;
-    snowflake.scale = 0.7f;
-    
-    // 添加粒子
     _snowLayer.emitterCells = @[snowflake];
     
     return _snowLayer;
